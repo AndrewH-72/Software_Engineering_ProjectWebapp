a61db1aed39aa3c5da4d012dbcd9a9b599a1ac8d
mariana1016 <157419980+mariana1016@users.noreply.github.com>
2025-05-14 10:30:58 -0500

    Update urls.py

--- webapp_project/todo_list/urls.py
+++ webapp_project/todo_list/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views  # Correct import statement
+from . import views  
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteTask
 
 urlpatterns = [
